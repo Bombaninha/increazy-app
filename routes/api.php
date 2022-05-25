a22e0238eccd33725b847e3bdcd832f3326496cd
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-05-25 00:53:12 -0300

    Adicionando nossa nova rota

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-//Route::get('{ceps}', [CepController::class, 'index']);
+Route::get('{ceps}', [CepController::class, 'index']);
